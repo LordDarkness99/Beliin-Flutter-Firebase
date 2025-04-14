f22045305e02a2ef9edc7db721cc6290e47e27d1
Abdul Haseeb <106963981+abdullhasseeb@users.noreply.github.com>
2025-04-14 11:36:33 +0500

    Update keys.dart

--- lib/utils/constants/keys.dart
+++ lib/utils/constants/keys.dart
@@ -7,11 +7,11 @@ class UKeys{
   static const String rememberMeEmail = 'rememberEmail';
   static const String rememberMePassword = 'rememberPassword';
 
-  static const String cloudName = 'dx4o5amce';
-  static const String uploadPreset = 'upload_image';
-  static const String profileFolder = 'Profile Pictures';
-  static const String apiKey = '578435728332811';
-  static const String apiSecret = 'h1oJYvp1ksY3v5ErlVetjvH8b8s';
+  static const String cloudName = '';
+  static const String uploadPreset = '';
+  static const String profileFolder = '';
+  static const String apiKey = '';
+  static const String apiSecret = '';
 
 
   static const String categoryFolder = 'Category Pictures';
